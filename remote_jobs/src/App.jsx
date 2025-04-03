dcda9f7bac74f00f54dbacee6afb5db04b349c2d
ee-rpeach <157056610+ee-rpeach@users.noreply.github.com>
2025-04-03 19:35:51 -0400

    inital commit

---
+++ remote_jobs/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>So you wanna work remote?</h1>
+      
+    </div>
+  )
+}
+
+export default App
